ed67c44caa78c1a68c413148d838377076d64299
Miracle121 <ravilovmirahmad@gmail.com>
2022-11-15 17:45:05 +0500

    app changed

--- models/interviewdate.js
+++ models/interviewdate.js
@@ -1,30 +1,73 @@
 const { Schema, model } = require('mongoose')
 const interviewdateSchema = new Schema({
-    
-    personsId:{
+
+    personsId: {
         type: Schema.Types.ObjectId,
         ref: 'Cntpeople',
         required: true
-    }, 
-
-    typeofinterviewdate:{
+    },
+    interviewId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Interview',
+        // required: true
+    },
+    typeofinterviewdate: {
         type: Schema.Types.ObjectId,
         ref: 'Typeofinterviewdate',
-        required: true      
-    },
-    dateofinterview: {
-        type: Date,
         required: true
     },
+    //oyda korinishida
+    duration:{
+        type: Number,
+        required: true
+
+    },
+
+    //Oxirgi suxbat vaqti
     lastdateofinterview: {
         type: Date,
         required: true
     },
+    nextdateofinterview: {
+        type: Date,
+        required: true
+    },
+    extradateofinterview: {
+        type: Date,
+        required: true
+    },
+    //Belgilangan vaqtdan o'tib ketsa yani nextdateofinterview dan joriy vaqt otgan bo'lsa
+    statusdateYellow: {
+        type: Date,
+        required: true
+    },
+    //Belgilangan vaqtdan o'tib ketsa yani statusdateyellow dan joriy vaqt +7 kundan oshgan bo'lsa bolsa otgan bo'lsa
+    statusdateRed: {
+        type: Date,
+        required: true
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     creatorId: {
         type: Schema.Types.ObjectId,
         ref: 'Users',
         required: true
     }
+
 },
     { timestamps: true })
 module.exports = model('Interviewdate', interviewdateSchema)
